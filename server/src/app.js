b3bdf4fe43c91059bb2cde7d6ca646d8408d3d00
Shahid Gillani <shahid5ssg@gmail.com>
2023-06-01 17:41:16 +0500

    These cors are getting me

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const api = require('./routes/api');
 const app = express();
 
 app.use(cors({
-  origin: 'https://localhost:8000',
+  origin: 'http://localhost:8000',
   
 }));
 app.use(morgan('combined'));
